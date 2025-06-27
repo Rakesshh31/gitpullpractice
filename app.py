f37c55a26c6fc84ecec5266df68ebf2626f76f7d
Rakesshh31 <rakesh.dyadav18@gmail.com>
2025-06-27 19:42:53 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+# Sample Python code
+
+# Print a greeting
+print("Hello, World!")
+
+# Define two numbers
+a = 5
+b = 3
+
+# Add the numbers
+sum = a + b
+
+# Display the result
+print("The sum of", a, "and", b, "is", sum)
